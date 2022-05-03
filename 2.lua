9a3ed6e67e6b285f42d1066531d0f44d5989512b
overlord of cum <88568085+ChenzIsHomo@users.noreply.github.com>
2022-05-03 20:32:40 +0100

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,10 +1,8 @@
-
-Example: chat alert roblox
 bc = BrickColor.new("White") -- Change white to the colour you want.
 bc2 = BrickColor.new("White") -- Change white to the colour you want.
 while true do
 	game.StarterGui:SetCore("ChatMakeSystemMessage", {
-		Text = "rawr!"; -- Change "MuppetIsEpic!" to the alert you want.
+		Text = "Nava is a homosexual"; -- Change "MuppetIsEpic!" to the alert you want.
 		Font = Enum.Font.Cartoon;
 		Color = bc.Color;
 		FontSize = Enum.FontSize.Size96
