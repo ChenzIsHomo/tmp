0feaf63a1699fc6cca23ea699beb8afbe23fc40d
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-05-03 15:27:34 -0400

    Update 4.lua

--- 4.lua
+++ 4.lua
@@ -1,19 +1,3 @@
-local playerLeaderstats = {}
-for i, v in pairs(game.Players:GetChildren()) do
-table.insert(playerLeaderstats, v)
-end
-for i, v in pairs(playerLeaderstats) do
-pe = Instance.new("ParticleEmitter",v.Character.Torso)
-pe.Texture = "http://www.roblox.com/asset/?id=8979672"
-pe.VelocitySpread = 999
-se = Instance.new("ParticleEmitter",v.Character.Torso)
-se.Texture = "http://www.roblox.com/asset/?id=176067516"
-se.VelocitySpread = 999
-fe = Instance.new("ParticleEmitter",v.Character.Torso)
-fe.Texture = "http://www.roblox.com/asset/?id=179012130"
-fe.VelocitySpread = 999
-end
-
 t1 = "http://www.roblox.com/asset/?id=223238256"
 t2 = "http://www.roblox.com/asset/?id=201788175"
 t3 = "http://www.roblox.com/asset/?id=261113277"
