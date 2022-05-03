7d66f1163f819077861880143d19ab3204331cd4
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-05-03 15:09:12 -0400

    Create 2.lua

---
+++ 2.lua
@@ -0,0 +1,12 @@
+vip = game.Players.gay
+
+function onChatted(msg)
+   if msg == "control/victims name" then
+game.Workspace.victims name:BreakJoint()
+local m = Instance.new("Message")
+m.Parent = game.Workspace
+m.Text = "woops look like it didnt work"
+end
+end
+
+vip.Chatted:connect(onChatted)
